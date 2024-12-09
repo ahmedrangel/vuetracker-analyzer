057d47974997a89cedf4fdd38d6f6d2bf5751bf1
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-08 22:18:35 -0500

    fix(playground): adjust user agent

--- playground/index.js
+++ playground/index.js
@@ -7,7 +7,7 @@ const router = AutoRouter();
 router.get("/vue-telescope/analyze?", async (req) => {
   const { url } = req.query;
   return await analyze(url, {
-    userAgent: "VueTracker/1.0 (Ubuntu 23.10; x64); https://vuetracker.pages.dev"
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 VueTracker/1.0 (Ubuntu 23.10; x64; vuetracker.nuxt.dev)"
   });
 });
 

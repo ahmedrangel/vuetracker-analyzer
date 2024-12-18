d4fdcca80d52420bf09fe168c7573a559b5384f5
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-18 17:02:18 -0500

    chore(playground): handle analyze error

--- playground/index.js
+++ playground/index.js
@@ -9,6 +9,8 @@ router.get("/vue-telescope/analyze?", async (req) => {
   const { url } = req.query;
   return await analyze(url, {
     userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 VueTracker/1.0 (Ubuntu 23.10; x64; vuetracker.nuxt.dev)"
+  }).catch((e) => {
+    return Response.json({ success: false, error: { message: e.message, cause: e.cause } }, { status: 500 });
   });
 });
 

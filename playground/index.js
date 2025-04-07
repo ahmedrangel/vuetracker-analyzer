b502e02444ae30c9d749dac51270d20307db3b3f
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-06 21:50:20 -0500

    chore(playground): adjust

--- playground/index.js
+++ playground/index.js
@@ -5,10 +5,10 @@ import { analyze } from "vuetracker-analyzer";
 
 const router = AutoRouter();
 
-router.get("/vue-telescope/analyze?", async (req) => {
+router.get("/analyze?", async (req) => {
   const { url } = req.query;
   return await analyze(url, {
-    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 VueTracker/1.0 (Ubuntu 23.10; x64; vuetracker.nuxt.dev)"
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 VueTracker/1.0 (Debian GNU/Linux 12; arm64; +https://vuetracker.nuxt.dev/)"
   }).catch((e) => {
     return Response.json({ success: false, error: { message: e.message, cause: e.cause } }, { status: 500 });
   });

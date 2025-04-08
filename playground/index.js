aff3ebc534318b16afb323962449b1be74dd9a08
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-07 20:12:53 -0500

    chore: adjust user agent

--- playground/index.js
+++ playground/index.js
@@ -8,7 +8,7 @@ const router = AutoRouter();
 router.get("/analyze?", async (req) => {
   const { url } = req.query;
   return await analyze(url, {
-    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 VueTracker/1.0 (Debian GNU/Linux 12; arm64; +https://vuetracker.nuxt.dev/)"
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 VueTracker/1.0 (Debian GNU/Linux 12; arm64; +vuetracker.nuxt.dev)"
   }).catch((e) => {
     return Response.json({ success: false, error: { message: e.message, cause: e.cause } }, { status: 500 });
   });
